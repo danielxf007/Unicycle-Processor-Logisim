4159d5edbc16137cc23b7620978ecf801c8e113e
danielxf007 <daniel.higuita007@gmail.com>
2020-07-20 20:18:18 -0500

    processor

--- FibonacciAssembly.asm
+++ FibonacciAssembly.asm
@@ -8,7 +8,7 @@ sw $t1, 4($sp)
 addi $sp, $sp, 12 # Se mueve el apuntador para almacenar el siguiente elemento
 addi $t3, $zero, 48 # Cantidad de elementos que faltan por ser almacenados
 LOOP: beq $t3, $zero, ENDLOOP # Se verifica que se termino de guardar los 50 numeros
-    addu $t2, $t1, $t0 # Se calcula el siguiente numero de Fibonacci, si se pone add hay overflow
+    add $t2, $t1, $t0 # Se calcula el siguiente numero de Fibonacci, si se pone add hay overflow
     sw $t2, 0($sp) # Se guarda en memoria el nuevo numero
     add $t0, $zero, $t1 # Se intercambian los valor de registro
     add $t1, $zero, $t2
